452d805d6289c08fbe3dc21e9d7f35457c9f9127
Code With Pooja <poojak2994@gmail.com>
2024-01-26 15:28:09 +0100

    Fixed the issue

--- src/main/java/com/example/mongo/MongoDbExampleApplication.java
+++ src/main/java/com/example/mongo/MongoDbExampleApplication.java
@@ -2,13 +2,10 @@ package com.example.mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@ComponentScan(basePackages = "com.example.mongo.repository")
+@SpringBootApplication
 @EnableSwagger2
 public class MongoDbExampleApplication {
 
--- src/main/java/com/example/mongo/service/PersonServiceImpl.java
+++ src/main/java/com/example/mongo/service/PersonServiceImpl.java
@@ -3,15 +3,21 @@ package com.example.mongo.service;
 import com.example.mongo.collection.Person;
 import com.example.mongo.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
 @Service
 
 public class PersonServiceImpl implements PersonService {
 
+
+    private final PersonRepository personRepository;
+
     @Autowired
-    private PersonRepository personRepository;
+    public PersonServiceImpl(PersonRepository personRepository)
+    {
+        this.personRepository = personRepository;
+    }
+
     @Override
     public String save(Person person) {
         return personRepository.save(person).getPersonId();
